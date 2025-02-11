5d428adfd6436b3e4a586578eeff7607fc090595
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2025-02-11 14:54:56 +0200

    add engine to games

---
+++ src/engine.js
@@ -0,0 +1,22 @@
+import readlineSync from 'readline-sync';
+
+const engine = (userName, rules, game) => {
+  console.log(rules);
+  const rounds = 3;
+  for (let i = 0; i < rounds; i += 1) {
+    const [question, answer] = game();
+    const userAnswer = readlineSync.question(`${question}\n`);
+    if (answer !== userAnswer) {
+      console.log(
+        `'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`
+      );
+      console.log(`Let's try again, ${userName}!`);
+      i = 0;
+    } else {
+      console.log('Correct!');
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default engine;
